978620376555ea84de1c9f053164a4ed8b424c79
david <davidszlafrok@gmail.com>
2021-03-30 01:42:02 +0300

    modified:   Task-Scehdular.ps1 	modified:   mytask.ps1 	deleted:    output.txt 	new file:   test.txt

--- Task-Scehdular.ps1
+++ Task-Scehdular.ps1
@@ -1,5 +1,20 @@
-
-param ([Parameter(Mandatory=$true)][String]$TaskName,[Parameter(Mandatory=$true)][int]$WaitSeconds)
+param ([String]$TaskName, [int]$WaitSeconds)
+$registerTaskParameters = @{
+    TaskName  =  $TaskName
+    }
+Function Create-Task (){
+$ScheduledTaskcreation = @{
+    Execute  = "pwsh.exe"
+    Argument = "-File C:\Users\david\Desktop\Sela_Files\Week2\Scripts\mytask.ps1"
+}
+$Action = New-ScheduledTaskAction 
+    -Execute 'pwsh.exe'
+    -Argument '-NonInteractive -NoLogo -NoProfile -File "C:\Users\david\Desktop\Sela_Files\Week2\Scripts\mytask.ps1"'
+$trigger = New-ScheduledTaskTrigger
+     -Once 
+     -At (Get-Date)
+     -RepetitionInterval (New-TimeSpan -Minutes 1)
+}
 
 Function Change-TaskStatus (){
     if( (Get-ScheduledTask | Where TaskName -eq $TaskName ).State -eq "Ready"){
@@ -9,44 +24,6 @@ Function Change-TaskStatus (){
             }
 }
 
-Function Create-Task (){
-
-$ScheduledTaskActionParams = @{
-    Execute  = "PowerShell.exe"
-    Argument = "-File C:\mytask.ps1"
-}
-
-$params = @{
-    Once = $true
-    At = (Get-Date)
-    RepetitionInterval = (New-TimeSpan -Minutes 1)
-}
-
-$registerTaskParameters = @{
-    Trigger   = New-ScheduledTaskTrigger  @params
-    TaskName  =  $TaskName
-    Action    = New-ScheduledTaskAction @ScheduledTaskActionParams
-    }
-
-Register-ScheduledTask @registerTaskParameters -Force
-}
-
-Function Get-AllTasks (){
- 
- $list=Get-ScheduledTask | Get-ScheduledTaskInfo
- Foreach($i in $list){
- echo $i.TaskName
- }
-}
-
-echo "Creating a new task with the name : $TaskName."
-echo "Task will run for $WaitSeconds seconds." 
-Create-Task
-$stopwatch = [System.Diagnostics.Stopwatch]::new()
-$stopwatch.Start()
-while ($stopwatch.Elapsed.TotalSeconds -lt $WaitSeconds){}
-echo "Stopping task $TaskName after $WaitSeconds."
-Change-TaskStatus
-$stopwatch.Stop()
-echo "All running tasks:"
-Get-AllTasks
+echo "Task will run for $WaitSeconds seconds."
+Change-TaskStatus 
+Get-ScheduledTask | Select-Object TaskName
